4dfb582fda8335a2b0e191c2238ac8d2f5326d34
Anandha Kumar <anandh2126@gmail.com>
2025-10-03 12:12:10 +0530

    Check Equal Arrays.py

---
+++ Check Equal Arrays.py
@@ -0,0 +1,18 @@
+Given two arrays a[] and b[] of equal size, the task is to find whether the elements in the arrays are equal.
+Two arrays are said to be equal if both contain the same set of elements, arrangements (or permutations) of elements may be different though.
+Note: If there are repetitions, then counts of repeated elements must also be the same for two arrays to be equal.
+
+Examples:
+
+Input: a[] = [1, 2, 5, 4, 0], b[] = [2, 4, 5, 0, 1]
+Output: true
+Explanation: Both the array can be rearranged to [0,1,2,4,5]
+
+_________________________________________________________________________________________________________________________________________________________________________
+
+class Solution:
+    def checkEqual(self, a, b):
+        if len(a) != len(b):
+            return False
+        
+        return sorted(a) == sorted(b)
